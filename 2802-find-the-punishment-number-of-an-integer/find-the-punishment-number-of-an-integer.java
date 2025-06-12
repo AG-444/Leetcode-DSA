233a2036393fdd811ddfce8e6425eead39cf566e
Aditya Garg <aditya444garg@gmail.com>
2025-06-12 23:36:46 +0530

    Time: 22 ms (69.45%) | Memory: 43.9 MB (68.55%) - LeetSync

---
+++ 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int punishmentNumber(int n) {
+        int total = 0;
+
+        for (int i = 1; i <= n; i++) {
+            int square = i * i;
+            if (canPartition(String.valueOf(square), 0, i)) {
+                total += square;
+            }
+        }
+
+        return total;
+    }
+
+    // Recursive backtracking function to check if numStr can be partitioned to sum up to target
+    private boolean canPartition(String numStr, int index, int target) {
+        if (index == numStr.length()) {
+            return target == 0;
+        }
+
+        int currentNum = 0;
+        for (int i = index; i < numStr.length(); i++) {
+            currentNum = currentNum * 10 + (numStr.charAt(i) - '0');
+            if (currentNum > target) break;
+            if (canPartition(numStr, i + 1, target - currentNum)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
