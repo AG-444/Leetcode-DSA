3d3f2b08ff5644c17f1857989895e3d71961eae5
Aditya Garg <aditya444garg@gmail.com>
2025-08-09 18:27:23 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (63.12%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,11 +1,6 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        double logValue = Math.log(n)/Math.log(2);
-        if (Math.abs(logValue - Math.round(logValue))< 1e-10){
-            return true;
-        }
-        else{
-            return false;
-        }
+        // n > 0 and only one bit set in binary representation
+        return n > 0 && (n & (n - 1)) == 0;
     }
 }
