1a04704b79cff4b7b3ddbc9877086eccc10757f0
Aditya Garg <aditya444garg@gmail.com>
2025-08-15 13:35:57 +0530

    Time: 456 ms (5.01%) | Memory: 48.2 MB (34.16%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> result = new ArrayList<>();
+        String[] strs2 = new String[strs.length];
+        for(int i =0 ;i<strs.length;i++){
+            char[] chs = strs[i].toCharArray();
+            Arrays.sort(chs);
+            strs2[i] = new String(chs);
+        }
+
+        for(int i=0;i<strs2.length;i++){
+            if(strs2[i] == "-1"){
+                continue;
+            }
+            List<String> ans = new ArrayList<>();
+            ans.add(strs[i]);
+            for(int j=i+1;j<strs2.length;j++){
+                if(strs2[i].equals(strs2[j])){
+                    ans.add(strs[j]);
+                    strs2[j] = "-1";
+                }
+            }
+            System.out.println(ans);
+            result.add(ans);
+        }
+        return result;
+    }
+}
