0bd96643d2b58b70a739a78f80b031b1b7b2d37a
Aditya Garg <aditya444garg@gmail.com>
2025-08-21 14:02:21 +0530

    Time: 5 ms (97.95%) | Memory: 45.1 MB (87.67%) - LeetSync

--- 1628-count-submatrices-with-all-ones/count-submatrices-with-all-ones.java
+++ 1628-count-submatrices-with-all-ones/count-submatrices-with-all-ones.java
@@ -2,25 +2,31 @@ class Solution {
     public int numSubmat(int[][] matrix) {
         int n = matrix.length;
         int m = matrix[0].length;
-        int[][] width = new int[n][m];
+        int[][] height = new int[n][m];
         int total = 0;
 
-        // Fill width array
-        for (int i = 0; i < n; i++) {
+        // Build heights for histogram
+        for (int j = 0; j < m; j++) {
+            height[0][j] = matrix[0][j];
+        }
+        for (int i = 1; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (matrix[i][j] == 1) {
-                    if (j == 0) {
-                        width[i][j] = 1;
-                    } else {
-                        width[i][j] = width[i][j-1] + 1;
-                    }
+                    height[i][j] = height[i-1][j] + 1;
+                } else {
+                    height[i][j] = 0;
+                }
+            }
+        }
 
-                    // Count rectangles ending at (i, j)
-                    int minWidth = width[i][j];
-                    for (int k = i; k >= 0 && minWidth > 0; k--) {
-                        minWidth = Math.min(minWidth, width[k][j]);
-                        total += minWidth;
-                    }
+        // For each row, count rectangles using heights
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                int minHeight = Integer.MAX_VALUE;
+                for (int k = j; k >= 0; k--) {
+                    if (height[i][k] == 0) break;
+                    minHeight = Math.min(minHeight, height[i][k]);
+                    total += minHeight;
                 }
             }
         }
