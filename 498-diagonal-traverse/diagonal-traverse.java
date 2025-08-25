cc661b4f4ce6fd1eff463b57ca7fd0ef6dad1fc8
Aditya Garg <aditya444garg@gmail.com>
2025-08-25 10:56:34 +0530

    Time: 89 ms (5.00%) | Memory: 47.5 MB (33.91%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.java
+++ 498-diagonal-traverse/diagonal-traverse.java
@@ -2,31 +2,55 @@ class Solution {
     public int[] findDiagonalOrder(int[][] mat) {
         int n = mat.length;
         int m = mat[0].length;
-        int total = n * m;
-        int[] result = new int[total];
+        int high = n+m-2;
+        if(n==1) return mat[0];
+        
+        ArrayList<Integer> arr = new ArrayList<>();
+        for(int i=0;i<=(high/2);i++){
+            System.out.println(i);
+            if(i%2==0){
+                for(int j=i;j>=0;j--){
+                    int k = i-j;
+                    if(j<n && k<m && k>=0 && j>=0){ 
+                        System.out.println("ADDED - "+j+" - "+k+" = "+mat[j][k]);
+                        arr.add(mat[j][k]);
+                    }
+                }
+            }
+            else{
+                for(int j=0;j<=i;j++){
+                    int k = i-j;
+                    if(j<n && k<m && k>=0 && j>=0) {
+                        System.out.println("ADDED - "+j+" - "+k+" = "+mat[j][k]);
+                        arr.add(mat[j][k]);
+                    }
+                }
+            }
+        }
         int idx = 0;
-
-        for (int i = 0; i < n + m - 1; i++) {
-            int row, col;
+        for (int i = (high/2)+1; i <= high; i++) {
             if (i % 2 == 0) {
-                // go up-right
-                row = Math.min(i, n - 1);
-                col = i - row;
+                int row = Math.min(i, n-1);
+                int col = i - row;
                 while (row >= 0 && col < m) {
-                    result[idx++] = mat[row][col];
+                    arr.add(mat[row][col]);
                     row--;
                     col++;
                 }
             } else {
-                // go down-left
-                col = Math.min(i, m - 1);
-                row = i - col;
+                int col = Math.min(i, m-1);
+                int row = i - col;
                 while (row < n && col >= 0) {
-                    result[idx++] = mat[row][col];
+                    arr.add(mat[row][col]);
                     row++;
                     col--;
                 }
             }
+        }
+        Integer[] boxedArr = arr.toArray(new Integer[0]);
+        int[] result = new int[boxedArr.length];
+        for (int i = 0; i < boxedArr.length; i++) {
+            result[i] = boxedArr[i];  // auto-unboxing
         }
         return result;
     }
