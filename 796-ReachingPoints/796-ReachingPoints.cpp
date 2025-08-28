0d7f17bef678a8a68c5fb6cdb735debc16355c1f
Aditya Garg <aditya444garg@gmail.com>
2025-08-28 09:57:58 +0530

    Create 796-ReachingPoints

---
+++ 796-ReachingPoints/796-ReachingPoints.cpp
@@ -0,0 +1,22 @@
+// Last updated: 8/28/2025, 9:57:58 AM
+class Solution {
+public:
+    bool reachingPoints(int sx, int sy, int tx, int ty) {
+        while (tx >= sx && ty >= sy) {
+            if (tx == sx && ty == sy) return true;
+
+            if (tx > ty) {
+                if (ty > sy)
+                    tx %= ty;
+                else  // ty == sy: we can only reduce tx by multiples of ty
+                    return (tx - sx) % ty == 0;
+            } else {
+                if (tx > sx)
+                    ty %= tx;
+                else  // tx == sx: reduce ty by multiples of tx
+                    return (ty - sy) % tx == 0;
+            }
+        }
+        return false;
+    }
+};
