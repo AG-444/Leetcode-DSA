770932c8f91abcf224d34a11379069c622778e2d
Aditya Garg <aditya444garg@gmail.com>
2025-08-28 09:58:54 +0530

    Create 18-4sum

---
+++ 18-4sum/18-4sum.java
@@ -0,0 +1,42 @@
+// Last updated: 8/28/2025, 9:58:55 AM
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        int n = nums.length;
+        Arrays.sort(nums);
+
+        for (int i = 0; i < n - 3; i++) {
+            // Skip duplicates for the first number
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            for (int j = i + 1; j < n - 2; j++) {
+                // Skip duplicates for the second number
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+
+                int left = j + 1;
+                int right = n - 1;
+
+                while (left < right) {
+                    long sum = (long)nums[i] + nums[j] + nums[left] + nums[right]; // use long to avoid overflow
+
+                    if (sum == target) {
+                        res.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+
+                        // Skip duplicates for the third and fourth numbers
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+                        while (left < right && nums[right] == nums[right - 1]) right--;
+
+                        left++;
+                        right--;
+                    } else if (sum < target) {
+                        left++; // Need a larger sum
+                    } else {
+                        right--; // Need a smaller sum
+                    }
+                }
+            }
+        }
+
+        return res;
+    }
+}
