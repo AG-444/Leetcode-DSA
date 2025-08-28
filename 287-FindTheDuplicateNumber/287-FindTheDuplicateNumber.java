8e755124d1e79a18eb70baf3a666e70b6efb7e3f
Aditya Garg <aditya444garg@gmail.com>
2025-08-28 09:58:10 +0530

    Create 287-FindTheDuplicateNumber

---
+++ 287-FindTheDuplicateNumber/287-FindTheDuplicateNumber.java
@@ -0,0 +1,22 @@
+// Last updated: 8/28/2025, 9:58:11 AM
+class Solution {
+    public int findDuplicate(int[] nums) {
+        // Step 1: Find the intersection point of the two runners.
+        int slow = nums[0];
+        int fast = nums[0];
+        
+        do {
+            slow = nums[slow];         // Move by 1 step
+            fast = nums[nums[fast]];   // Move by 2 steps
+        } while (slow != fast);
+        
+        // Step 2: Find the entrance to the cycle (duplicate number)
+        slow = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        
+        return slow;
+    }
+}
