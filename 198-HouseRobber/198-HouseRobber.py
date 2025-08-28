cbeb747b7b1ed508eed405646ab29b6aff9d8542
Aditya Garg <aditya444garg@gmail.com>
2025-08-28 09:58:19 +0530

    Create 198-HouseRobber

---
+++ 198-HouseRobber/198-HouseRobber.py
@@ -0,0 +1,24 @@
+# Last updated: 8/28/2025, 9:58:19 AM
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        
+        if len(nums) == 1:
+            return nums[0]
+        
+        # Initialize dp array
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        
+        # Fill the dp array
+        for i in range(2, len(nums)):
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+        
+        # The last element in dp will have the maximum amount that can be robbed
+        return dp[-1]
