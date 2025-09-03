fa78bbb1e527fa255cf95b30800e7a86c65f3eb9
Aditya Garg <aditya444garg@gmail.com>
2025-09-03 16:20:49 +0530

    File updated at 9/3/2025, 4:20:46 PM

--- 3025-FindtheNumberofWaystoPlacePeopleI/3025-FindtheNumberofWaystoPlacePeopleI.java
+++ 3025-FindtheNumberofWaystoPlacePeopleI/3025-FindtheNumberofWaystoPlacePeopleI.java
@@ -1,23 +1,36 @@
-// Last updated: 9/2/2025, 10:01:12 AM
+// Last updated: 9/3/2025, 4:20:46 PM
 class Solution {
     public int numberOfPairs(int[][] points) {
         int count = 0;
         int n = points.length;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if((points[j][0] - points[i][0])<=0 && (points[j][1] - points[i][1])>=0){
-                    if(i==j) continue;
-                    boolean check = true;
-                    // System.out.println("i: " + Arrays.toString(points[i]) + ", j: " + Arrays.toString(points[j]));
-                    for(int k=0;k<n;k++){
-                        if(k==i||k==j) continue;
-                        if(points[k][0]>=points[j][0] && points[k][0]<=points[i][0] && points[k][1]<=points[j][1] && points[k][1]>= points[i][1]){
-                            // System.out.println("i: " + Arrays.toString(points[i]) + ", j: " + Arrays.toString(points[j])+ " TEST - "+Arrays.toString(points[k]));
-                            check = false;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j) continue;
+
+                // check orientation first
+                if (points[i][0] >= points[j][0] && points[i][1] <= points[j][1]) {
+
+                    // bounding box limits
+                    int xLow = points[j][0];
+                    int xHigh = points[i][0];
+                    int yLow = points[i][1];
+                    int yHigh = points[j][1];
+
+                    boolean valid = true;
+
+                    for (int k = 0; k < n; k++) {
+                        if (k == i || k == j) continue;
+
+                        int x = points[k][0];
+                        int y = points[k][1];
+
+                        if (x >= xLow && x <= xHigh && y >= yLow && y <= yHigh) {
+                            valid = false;
                             break;
                         }
                     }
-                    if(check) count++;
+                    if (valid) count++;
                 }
             }
         }
