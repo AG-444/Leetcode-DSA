8e9601170ceed90932d1c326c9b74423468b9a9b
Aditya Garg <aditya444garg@gmail.com>
2025-09-03 17:13:30 +0530

    File updated at 9/3/2025, 5:13:27 PM

--- 75-SortColors/75-SortColors.java
+++ 75-SortColors/75-SortColors.java
@@ -1,26 +1,6 @@
-// Last updated: 8/28/2025, 9:58:36 AM
+// Last updated: 9/3/2025, 5:13:27 PM
 class Solution {
     public void sortColors(int[] nums) {
-        int low = 0, mid = 0, high = nums.length - 1;
-
-        while (mid <= high) {
-            if (nums[mid] == 0) {
-                // Swap 0 to the front
-                int temp = nums[low];
-                nums[low] = nums[mid];
-                nums[mid] = temp;
-                low++;
-                mid++;
-            } else if (nums[mid] == 1) {
-                // 1 is in the middle, just move mid
-                mid++;
-            } else {
-                // Swap 2 to the end
-                int temp = nums[mid];
-                nums[mid] = nums[high];
-                nums[high] = temp;
-                high--;
-            }
-        }
+        Arrays.sort(nums);
     }
 }
