721748bdfeb0991de56e8fc22835f18fd38e1f48
Aditya Garg <aditya444garg@gmail.com>
2025-09-05 20:24:27 +0530

    File updated at 9/5/2025, 8:24:23 PM

--- 17-LetterCombinationsofaPhoneNumber/17-LetterCombinationsofaPhoneNumber.java
+++ 17-LetterCombinationsofaPhoneNumber/17-LetterCombinationsofaPhoneNumber.java
@@ -1,35 +1,37 @@
-// Last updated: 9/5/2025, 7:33:45 PM
+// Last updated: 9/5/2025, 8:24:23 PM
 class Solution {
-    static Map<Character,String> map = new HashMap<>();
-    {
-        map.put('2',"abc");
-        map.put('3',"def");
-        map.put('4',"ghi");
-        map.put('5',"jkl");
-        map.put('6',"mno");
-        map.put('7',"pqrs");
-        map.put('8',"tuv");
-        map.put('9',"wxyz");
+    public List<String> letterCombinations(String digits) {
+        List<String> results = new ArrayList<>();
+        if (digits.length() == 0) {
+            return results;
+        }
+        
+        Map<Character, char[]> digitToLetters = new HashMap<>(8);
+        digitToLetters.put('2', new char[]{ 'a', 'b', 'c' });
+        digitToLetters.put('3', new char[]{ 'd', 'e', 'f' });
+        digitToLetters.put('4', new char[]{ 'g', 'h', 'i' });
+        digitToLetters.put('5', new char[]{ 'j', 'k', 'l' });
+        digitToLetters.put('6', new char[]{ 'm', 'n', 'o' });
+        digitToLetters.put('7', new char[]{ 'p', 'q', 'r', 's' });
+        digitToLetters.put('8', new char[]{ 't', 'u', 'v' });
+        digitToLetters.put('9', new char[]{ 'w', 'x', 'y', 'z' });
+
+        buildLetterCombinations(digits, 0, digitToLetters, new StringBuilder(digits.length()), results);
+
+        return results;
     }
-    public void backtrack(String digits, int idx, StringBuilder curr, List<String> result){
-        if(idx == digits.length()){
-            String s = curr.toString();
-            result.add(s);
+
+    private void buildLetterCombinations(String digits, int digitIndex, Map<Character, char[]> digitToLetters, StringBuilder builder, List<String> results) {
+        if (digitIndex >= digits.length()) {
+            results.add(builder.toString());
             return;
         }
 
-        String a = map.get(digits.charAt(idx));
-        for(char ch:a.toCharArray()){
-            curr.append(ch);
-            backtrack(digits,idx+1,curr,result);
-            curr.deleteCharAt(curr.length()-1);
+        char digit = digits.charAt(digitIndex);
+        for (char letter : digitToLetters.get(digit)) {
+            builder.append(letter);
+            buildLetterCombinations(digits, digitIndex + 1, digitToLetters, builder, results);
+            builder.deleteCharAt(builder.length() - 1);
         }
-
-    }
-    public List<String> letterCombinations(String digits) {
-        List<String> ans = new ArrayList<>();
-        if(digits.length()==0) return ans;
-        backtrack(digits, 0 ,new StringBuilder(), ans);
-        return  ans;
     }
 }
