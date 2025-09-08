e27e2015f8f92e5e70902a0b25934ee1e1ca4f37
Aditya Garg <aditya444garg@gmail.com>
2025-09-08 18:58:52 +0530

    File updated at 9/8/2025, 6:58:49 PM

--- 1317-ConvertIntegertotheSumofTwoNo-ZeroIntegers/1317-ConvertIntegertotheSumofTwoNo-ZeroIntegers.java
+++ 1317-ConvertIntegertotheSumofTwoNo-ZeroIntegers/1317-ConvertIntegertotheSumofTwoNo-ZeroIntegers.java
@@ -1,14 +1,24 @@
-// Last updated: 9/8/2025, 6:57:29 PM
+// Last updated: 9/8/2025, 6:58:49 PM
 class Solution {
     public int[] getNoZeroIntegers(int n) {
-        int[] ans = new int[2];
-        for(int i=1;i<n;i++){
-            Character[] a = String.valueOf(n - i).chars().mapToObj(c -> (char) c).toArray(Character[]::new);
-            Character[] b = String.valueOf(i).chars().mapToObj(c -> (char) c).toArray(Character[]::new);
-            if(!Arrays.asList(a).contains('0') && !Arrays.asList(b).contains('0')){
-                ans =  new int[]{i,n-i};
+      
+  
+       for(int i=1;i<=n;i++){
+            int a = i;
+            int b = n-i;
+            if(a>0 && b>0 && iscontainsZero(a) == false && iscontainsZero(b) == false){
+                return new int[]{a,b};
             }
+       }
+    
+    return new int[] {-1,-1};
+    }
+    public boolean iscontainsZero(int n){
+        while(n > 0){
+            int digits = n % 10;
+            if(digits == 0) return true;
+            n /= 10;
         }
-        return ans;
+        return false;
     }
 }
