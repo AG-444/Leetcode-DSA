2bf079488208a771fd4248d9868eb999dd855f69
Aditya Garg <aditya444garg@gmail.com>
2025-09-08 14:08:35 +0530

    File updated at 9/8/2025, 2:08:33 PM

--- 84-LargestRectangleinHistogram/84-LargestRectangleinHistogram.java
+++ 84-LargestRectangleinHistogram/84-LargestRectangleinHistogram.java
@@ -1,24 +1,20 @@
-// Last updated: 9/8/2025, 2:06:41 PM
+// Last updated: 9/8/2025, 2:08:33 PM
 class Solution {
-    public int largestRectangleArea(int[] height){
-      int[] heights = new int[height.length+1];
-      for(int i=0;i<height.length;i++){
-        heights[i] = height[i];
-      }
-      heights[height.length] = 0;
-      
-      Stack<Integer> stack = new Stack<>();
-      int maxArea = 0;
-      for(int i=0;i<heights.length;i++){
-        while(!stack.isEmpty() && heights[stack.peek()] > heights[i]){
-            int curr = stack.peek();
-            stack.pop();
-            int pse = stack.isEmpty() ? -1:stack.peek();
-            int area = heights[curr] * (i - pse - 1);
-            if(area > maxArea) maxArea = area;
+    public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        int maxArea = 0;
+        Deque<Integer> stack = new ArrayDeque<>();
+        
+        for (int i = 0; i <= n; i++) {
+            int h = (i == n) ? 0 : heights[i]; // sentinel at end
+            while (!stack.isEmpty() && heights[stack.peek()] > h) {
+                int heightIndex = stack.pop();
+                int width = stack.isEmpty() ? i : i - stack.peek() - 1;
+                maxArea = Math.max(maxArea, heights[heightIndex] * width);
+            }
+            stack.push(i);
         }
-        stack.push(i);
-      }
-      return maxArea;
+        
+        return maxArea;
     }
 }
