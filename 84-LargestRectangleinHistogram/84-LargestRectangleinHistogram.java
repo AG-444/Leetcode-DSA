b31a40e33125a6e8ea86e8eee75a50cd4b127ac4
Aditya Garg <aditya444garg@gmail.com>
2025-09-08 14:06:42 +0530

    File updated at 9/8/2025, 2:06:41 PM

--- 84-LargestRectangleinHistogram/84-LargestRectangleinHistogram.java
+++ 84-LargestRectangleinHistogram/84-LargestRectangleinHistogram.java
@@ -1,4 +1,4 @@
-// Last updated: 9/8/2025, 12:07:40 PM
+// Last updated: 9/8/2025, 2:06:41 PM
 class Solution {
     public int largestRectangleArea(int[] height){
       int[] heights = new int[height.length+1];
@@ -6,39 +6,19 @@ class Solution {
         heights[i] = height[i];
       }
       heights[height.length] = 0;
-      int[] nse = new int[heights.length];
-      for(int i=0;i<heights.length;i++){
-        nse[i] = -1;  
-      }
-      Stack<Integer> stack = new Stack<>();
-      stack.push(0);
-      for(int i=1;i<heights.length;i++){
-        while(!stack.isEmpty() && heights[stack.peek()]>heights[i]){
-            nse[stack.peek()] = i;
-            stack.pop();
-        }
-        stack.push(i);
-      }
       
-      stack.clear();
-      int[] pse = new int[heights.length];
+      Stack<Integer> stack = new Stack<>();
+      int maxArea = 0;
       for(int i=0;i<heights.length;i++){
-        while(!stack.isEmpty() && heights[stack.peek()] >= heights[i]){
+        while(!stack.isEmpty() && heights[stack.peek()] > heights[i]){
+            int curr = stack.peek();
             stack.pop();
+            int pse = stack.isEmpty() ? -1:stack.peek();
+            int area = heights[curr] * (i - pse - 1);
+            if(area > maxArea) maxArea = area;
         }
-        pse[i] = stack.isEmpty()? -1:stack.peek();
         stack.push(i);
       }
-
-      for(int i=0;i<heights.length;i++){
-        System.out.println(heights[i]+ " " + nse[i]);
-        System.out.println(heights[i]+ " " + pse[i]+"\n");
-      }
-      int max = 0;
-      for(int i =0;i<height.length;i++){
-        int area = heights[i]*(nse[i] - pse[i] - 1);
-        if(area>max) max =area;
-      }
-      return max;
+      return maxArea;
     }
 }
