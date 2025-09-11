e50e7a123b389ef8e459f9904c0feea24a1573bc
Aditya Garg <aditya444garg@gmail.com>
2025-09-11 16:10:10 +0530

    File updated at 9/11/2025, 4:10:07 PM

--- 532-K-diffPairsinanArray/532-K-diffPairsinanArray.java
+++ 532-K-diffPairsinanArray/532-K-diffPairsinanArray.java
@@ -1,31 +1,56 @@
-// Last updated: 9/11/2025, 4:08:26 PM
+// Last updated: 9/11/2025, 4:10:07 PM
 class Solution {
     public int findPairs(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for(int n:nums){
-            map.put(n,map.getOrDefault(n,0)+1);
+        if(nums.length==0) return 0;
+
+        return findP(nums,k);
+    }
+
+    protected int findP(int[] nums, int k)
+    {
+      Map<Integer,Integer> map = new HashMap<>();
+      int counter=0;
+
+      for(int i=0;i<nums.length;i++)
+      {
+        map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+      }
+
+      for(Map.Entry<Integer,Integer> entry:map.entrySet())
+      { 
+        int val = entry.getKey();
+        int ke = val+k; //5
+       // printMap(map);
+       
+        if(k>0 && map.containsKey(ke))
+        {
+            //System.out.println("The nums "+val);
+             //System.out.println("The key "+ke);             
+           counter++;       
+           
         }
-        System.out.println(map);
+        else if((k==0) &&(map.get(ke)>1))
+        {
+            //System.out.println("In: The nums "+val);
+             //System.out.println("In : The key "+ke);            
+                counter++;
+        }       
+      }
+      return counter;
+       
+    }
 
-        int count=0;
-        if(k==0){
-            for(Map.Entry<Integer, Integer> entry : map.entrySet()){
-                if(map.get(entry.getKey()+k)>=2){
-                    count++;
-                }
-            }
+    protected void printMap(Map<Integer,Integer> map)
+    {
+        for(Map.Entry<Integer,Integer> entry:map.entrySet())
+        {
+            int key = entry.getKey();
+            int val = entry.getValue();
+            
+               System.out.print(" key->" +key);
+           
+            System.out.println(" value "+val);
+            
         }
-        else{
-            for(Map.Entry<Integer, Integer> entry: map.entrySet()){
-                if(map.containsKey(entry.getKey()+k)){
-                    count++;
-                }
-            }
-
-        }
-
-
-
-        return count;
     }
 }
