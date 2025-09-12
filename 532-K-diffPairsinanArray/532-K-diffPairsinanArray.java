41481de4ea94bdd932d98b0576f2cffe257dddde
Aditya Garg <aditya444garg@gmail.com>
2025-09-12 11:46:04 +0530

    File updated at 9/12/2025, 11:46:03 AM

--- 532-K-diffPairsinanArray/532-K-diffPairsinanArray.java
+++ 532-K-diffPairsinanArray/532-K-diffPairsinanArray.java
@@ -1,56 +1,27 @@
-// Last updated: 9/11/2025, 4:13:22 PM
+// Last updated: 9/12/2025, 11:46:03 AM
 class Solution {
     public int findPairs(int[] nums, int k) {
-        if(nums.length==0) return 0;
+        if(k < 0) return 0;
 
-        return findP(nums,k);
-    }
-
-    protected int findP(int[] nums, int k)
-    {
-      Map<Integer,Integer> map = new HashMap<>();
-      int counter=0;
-
-      for(int i=0;i<nums.length;i++)
-      {
-        map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-      }
-
-      for(Map.Entry<Integer,Integer> entry:map.entrySet())
-      { 
-        int val = entry.getKey();
-        int ke = val+k; //5
-       // printMap(map);
-       
-        if(k>0 && map.containsKey(ke))
-        {
-            //System.out.println("The nums "+val);
-             //System.out.println("The key "+ke);             
-           counter++;       
-           
-        }
-        else if((k==0) &&(map.get(ke)>1))
-        {
-            //System.out.println("In: The nums "+val);
-             //System.out.println("In : The key "+ke);            
-                counter++;
-        }       
-      }
-      return counter;
-       
-    }
-
-    protected void printMap(Map<Integer,Integer> map)
-    {
-        for(Map.Entry<Integer,Integer> entry:map.entrySet())
-        {
-            int key = entry.getKey();
-            int val = entry.getValue();
-            
-               System.out.print(" key->" +key);
-           
-            System.out.println(" value "+val);
-            
+        if(k == 0){
+            Map<Integer, Integer> freq = new HashMap<>();
+            int count = 0;
+            for(int n : nums){
+                freq.put(n, freq.getOrDefault(n, 0) + 1);
+            }
+            for(int val : freq.values()){
+                if(val >= 2) count++;
+            }
+            return count;
+        } else {
+            Set<Integer> set = new HashSet<>();
+            Set<Integer> seen = new HashSet<>();
+            int count = 0;
+            for(int n : nums) set.add(n);
+            for(int n : set){
+                if(set.contains(n + k)) count++;
+            }
+            return count;
         }
     }
 }
