41b021c0c2a8c9b1d4f798bd626b70ff86135936
Aditya Garg <aditya444garg@gmail.com>
2025-09-12 07:39:07 +0530

    File updated at 9/12/2025, 7:39:04 AM

--- 3227-VowelsGameinaString/3227-VowelsGameinaString.java
+++ 3227-VowelsGameinaString/3227-VowelsGameinaString.java
@@ -1,15 +1,10 @@
-// Last updated: 9/12/2025, 7:38:30 AM
+// Last updated: 9/12/2025, 7:39:04 AM
 class Solution {
     public boolean doesAliceWin(String s) {
-        int count = 0;
-        for (int i = 0; i < s.length(); i++) {
-            char ch = Character.toLowerCase(s.charAt(i));
-            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
-                count++;
-            }
-        }
-        if(count == 0) return false;
-        else if(count%2 == 1) return true;
-        else return true;
+        for(int i=0;i<s.length();i++){
+            int ch=s.charAt(i);
+            if(ch=='a' || ch=='e' ||ch=='i' ||ch=='o' ||ch=='u') return true;
+
+        } return false;
     }
 }
