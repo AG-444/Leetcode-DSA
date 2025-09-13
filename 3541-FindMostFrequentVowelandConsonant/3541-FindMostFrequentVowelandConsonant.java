ec0e6d003bb86349a2b55cfdb0967d9525e9d7bb
Aditya Garg <aditya444garg@gmail.com>
2025-09-13 12:36:06 +0530

    File updated at 9/13/2025, 12:36:06 PM

--- 3541-FindMostFrequentVowelandConsonant/3541-FindMostFrequentVowelandConsonant.java
+++ 3541-FindMostFrequentVowelandConsonant/3541-FindMostFrequentVowelandConsonant.java
@@ -1,20 +1,20 @@
-// Last updated: 9/13/2025, 12:34:35 PM
+// Last updated: 9/13/2025, 12:36:06 PM
 class Solution {
     public int maxFreqSum(String s) {
         int[] alpha = new int[26];
         for(int i=0;i<s.length();i++){
             alpha[s.charAt(i)-'a']++;
         }
-        int maxVowel = Collections.max(Arrays.asList(alpha[0],alpha[4],alpha[8],alpha[14],alpha[20]));
         
-        alpha[0]=0;
-        alpha[4]=0;
-        alpha[8]=0;
-        alpha[14]=0;
-        alpha[20]=0;
-
-        int max = Arrays.stream(alpha).max().getAsInt();
-
-        return maxVowel+max;
+        int maxVowel = 0;
+        int maxConsonant = 0;
+        for (int i = 0; i < 26; i++) {
+            if (i == 0 || i == 4 || i == 8 || i == 14 || i == 20) {
+                maxVowel = Math.max(maxVowel, alpha[i]);
+            } else {
+                maxConsonant = Math.max(maxConsonant, alpha[i]);
+            }
+        }
+        return maxVowel+maxConsonant;
     }
 }
