49e2666a1e72249554199756af51a194441cf623
Aditya Garg <aditya444garg@gmail.com>
2025-09-14 11:56:30 +0530

    File updated at 9/14/2025, 11:56:29 AM

--- 686-RepeatedStringMatch/686-RepeatedStringMatch.java
+++ 686-RepeatedStringMatch/686-RepeatedStringMatch.java
@@ -1,17 +1,20 @@
-// Last updated: 9/14/2025, 11:53:34 AM
+// Last updated: 9/14/2025, 11:56:29 AM
 class Solution {
     public int repeatedStringMatch(String a, String b) {
         StringBuilder sb = new StringBuilder();
         int i=0;
-        while(sb.length()<=10000){
-            if(sb.indexOf(b) == -1){
-                sb.append(a);
-                i++;
-            }
-            else{
-                return i;
-            }
+        while(sb.length()<b.length()){
+            i++;
+            sb.append(a);
         }
+
+        if(sb.indexOf(b) != -1) return i;
+
+        sb.append(a);
+        i++;
+        if(sb.indexOf(b)!=-1) return i;
+
+        
         return -1;
     }
 }
