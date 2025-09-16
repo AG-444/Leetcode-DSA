8f050467d1b577457054b9aa8cfb48be1e1a5d31
Aditya Garg <aditya444garg@gmail.com>
2025-09-16 22:52:26 +0530

    File updated at 9/16/2025, 10:52:25 PM

--- 17-LetterCombinationsofaPhoneNumber/17-LetterCombinationsofaPhoneNumber.java
+++ 17-LetterCombinationsofaPhoneNumber/17-LetterCombinationsofaPhoneNumber.java
@@ -1,6 +1,6 @@
-// Last updated: 9/16/2025, 10:20:25 PM
+// Last updated: 9/16/2025, 10:52:25 PM
 class Solution {
-    static Map<Character,String> map = new HashMap<>();
+    public Map<Character,String> map = new HashMap<>();
     {
         map.put('2',"abc");
         map.put('3',"def");
@@ -11,25 +11,23 @@ class Solution {
         map.put('8',"tuv");
         map.put('9',"wxyz");
     }
-    public void backtrack(String digits, int idx, StringBuilder curr, List<String> result){
-        if(idx == digits.length()){
-            String s = curr.toString();
-            result.add(s);
+    public void backTrack(String digits, int idx, StringBuilder curr, List<String> ans){
+        if(digits.length() == idx){
+            ans.add(new String(curr.toString()));
             return;
         }
-
-        String a = map.get(digits.charAt(idx));
-        for(char ch:a.toCharArray()){
+        String s = map.get(digits.charAt(idx));
+        for(char ch: s.toCharArray()){
             curr.append(ch);
-            backtrack(digits,idx+1,curr,result);
+            backTrack(digits, idx+1, curr, ans);
             curr.deleteCharAt(curr.length()-1);
         }
-
     }
+
     public List<String> letterCombinations(String digits) {
         List<String> ans = new ArrayList<>();
         if(digits.length()==0) return ans;
-        backtrack(digits, 0 ,new StringBuilder(), ans);
-        return  ans;
+        backTrack(digits,0,new StringBuilder(),ans);
+        return (ans);
     }
 }
