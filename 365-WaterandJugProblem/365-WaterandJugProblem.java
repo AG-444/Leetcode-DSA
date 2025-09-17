01862f8dceaf2361c20da3db0e7f857f925ceb82
Aditya Garg <aditya444garg@gmail.com>
2025-09-17 09:49:42 +0530

    File updated at 9/17/2025, 9:49:40 AM

--- 365-WaterandJugProblem/365-WaterandJugProblem.java
+++ 365-WaterandJugProblem/365-WaterandJugProblem.java
@@ -1,28 +1,24 @@
-// Last updated: 9/17/2025, 9:46:58 AM
+// Last updated: 9/17/2025, 9:49:40 AM
 class Solution {
     public boolean canMeasureWater(int x, int y, int target) {
-        return canMeasureWater(x, y, x, y, target, new HashSet<>());
-    }
-
-    public boolean canMeasureWater(int x, int y, int xCapacity, int yCapacity, int target, Set<String> visited) {
-        String key = getKey(x, y);
-        if(visited.contains(key)) {
+        if (target > x + y){
             return false;
         }
-        visited.add(key);
-        if(x == target || y == target || x + y == target) {
+        
+        if (target == 0){
             return true;
         }
-        return canMeasureWater(0, y, xCapacity, yCapacity, target, visited) 
-            || canMeasureWater(x, 0, xCapacity, yCapacity, target, visited)
-            || canMeasureWater(xCapacity, y, xCapacity, yCapacity, target, visited)
-            || canMeasureWater(x, yCapacity, xCapacity, yCapacity, target, visited)
-            || canMeasureWater(Math.max(0, x - (yCapacity-y)), Math.min(x+y, yCapacity), xCapacity, yCapacity, target, visited)
-            || canMeasureWater(Math.min(x+y, xCapacity), Math.max(0, y - (xCapacity-x)), xCapacity, yCapacity, target, visited);
         
+        return target % gcd(x, y) == 0;
     }
 
-    private String getKey(int x, int y) {
-        return new StringBuilder().append(x).append("-").append(y).toString();
+    private int gcd(int x, int y) {
+        while (y != 0){
+            int temp = y;
+            y = x % y;
+            x = temp;
+        }
+        
+        return x;
     }
 }
