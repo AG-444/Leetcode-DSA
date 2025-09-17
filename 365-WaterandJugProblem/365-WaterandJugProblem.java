dfbf7106d80bd2733160d8b05e53cd803401f897
Aditya Garg <aditya444garg@gmail.com>
2025-09-17 09:47:00 +0530

    File updated at 9/17/2025, 9:46:58 AM

--- 365-WaterandJugProblem/365-WaterandJugProblem.java
+++ 365-WaterandJugProblem/365-WaterandJugProblem.java
@@ -1,42 +1,28 @@
-// Last updated: 9/17/2025, 9:45:19 AM
+// Last updated: 9/17/2025, 9:46:58 AM
 class Solution {
     public boolean canMeasureWater(int x, int y, int target) {
-        Queue<Integer> queue = new LinkedList<>();
-        Set<Integer> set = new HashSet<>();
-        queue.add(0);
-        set.add(0);
-        while(!queue.isEmpty()){
-            int a = queue.poll();
-            System.out.println(a);
-            if(a+x>0 && a+x<=x+y){
-                if(a+x == target) return true;
-                if(!set.contains(a+x)){
-                    set.add(a+x);
-                    queue.add(a+x);
-                };
-            }
-            if(a+y>0 && a+y<=x+y){
-                if(a+y == target) return true;
-                if(!set.contains(a+y)){
-                    set.add(a+y);
-                    queue.add(a+y);
-                };
-            }
-            if(a-x>0 && a-x<=x+y){
-                if(a-x == target) return true;
-                if(!set.contains(a-x)){
-                    set.add(a-x);
-                    queue.add(a-x);
-                };
-            }
-            if(a-y>0 && a-y<=x+y){
-                if(a-y == target) return true;
-                if(!set.contains(a-y)){
-                    set.add(a-y);
-                    queue.add(a-y);
-                };
-            }
+        return canMeasureWater(x, y, x, y, target, new HashSet<>());
+    }
+
+    public boolean canMeasureWater(int x, int y, int xCapacity, int yCapacity, int target, Set<String> visited) {
+        String key = getKey(x, y);
+        if(visited.contains(key)) {
+            return false;
         }
-        return false;
+        visited.add(key);
+        if(x == target || y == target || x + y == target) {
+            return true;
+        }
+        return canMeasureWater(0, y, xCapacity, yCapacity, target, visited) 
+            || canMeasureWater(x, 0, xCapacity, yCapacity, target, visited)
+            || canMeasureWater(xCapacity, y, xCapacity, yCapacity, target, visited)
+            || canMeasureWater(x, yCapacity, xCapacity, yCapacity, target, visited)
+            || canMeasureWater(Math.max(0, x - (yCapacity-y)), Math.min(x+y, yCapacity), xCapacity, yCapacity, target, visited)
+            || canMeasureWater(Math.min(x+y, xCapacity), Math.max(0, y - (xCapacity-x)), xCapacity, yCapacity, target, visited);
+        
+    }
+
+    private String getKey(int x, int y) {
+        return new StringBuilder().append(x).append("-").append(y).toString();
     }
 }
