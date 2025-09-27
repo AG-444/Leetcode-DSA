4d89c59f659ffaa488f5f8c59eed03038bd6c989
Aditya Garg <aditya444garg@gmail.com>
2025-09-27 14:24:16 +0530

    File updated at 9/27/2025, 2:24:14 PM

--- 812-LargestTriangleArea/812-LargestTriangleArea.java
+++ 812-LargestTriangleArea/812-LargestTriangleArea.java
@@ -1,21 +1,26 @@
-// Last updated: 9/27/2025, 2:23:17 PM
+// Last updated: 9/27/2025, 2:24:14 PM
 class Solution {
     public double largestTriangleArea(int[][] points) {
         int n = points.length;
         double maxArea = 0;
 
-        for(int i=0;i<n;i++){
-            double x1 = points[i][0], y1 = points[i][1];
-            for(int j=0;j<n;j++){
-                double x2 = points[j][0], y2 = points[j][1];
-                for(int k=0;k<n;k++){
-                    double x3 = points[k][0], y3 = points[k][1];
-                    maxArea = Math.max(maxArea , 0.5*(Math.abs(x1*(y2 - y3) + x2*(y3 - y1) + x3*(y1 - y2))));
+        for (int i = 0; i < n; i++) {
+            int x1 = points[i][0], y1 = points[i][1];
+            for (int j = i + 1; j < n; j++) {
+                int x2 = points[j][0], y2 = points[j][1];
+                for (int k = j + 1; k < n; k++) {
+                    int x3 = points[k][0], y3 = points[k][1];
+
+                    int determinant = x1 * (y2 - y3) 
+                                    + x2 * (y3 - y1) 
+                                    + x3 * (y1 - y2);
+
+                    double area = 0.5 * Math.abs(determinant);
+                    maxArea = Math.max(maxArea, area);
                 }
             }
         }
 
         return maxArea;
-
     }
 }
