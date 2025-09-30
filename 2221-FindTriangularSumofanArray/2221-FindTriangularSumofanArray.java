094c90b2ad17aa62b9b06ab6ff818f2d5effc3c7
Aditya Garg <aditya444garg@gmail.com>
2025-09-30 20:04:18 +0530

    File updated at 9/30/2025, 8:04:14 PM

--- 2221-FindTriangularSumofanArray/2221-FindTriangularSumofanArray.java
+++ 2221-FindTriangularSumofanArray/2221-FindTriangularSumofanArray.java
@@ -1,24 +1,15 @@
-// Last updated: 9/30/2025, 8:02:11 PM
+// Last updated: 9/30/2025, 8:04:14 PM
 class Solution {
     public int triangularSum(int[] nums) {
-        int ans = 0;
-        Queue<Integer> q1 = new LinkedList<>();
-        for (int n : nums) {
-            q1.offer(n);
-        }
-        Queue<Integer> q2 = new LinkedList<>();
-        while(q1.size() > 1){    
-            int size = q1.size();
-            for (int i = 0; i < size - 1; i++) {
-                int a = q1.poll();
-                int b = q1.peek();
-                q2.offer((a+b)%10);
+        int n = nums.length;
+
+        while (n > 1) { // keep shrinking until only one number left
+            for (int i = 0; i < n - 1; i++) {
+                nums[i] = (nums[i] + nums[i + 1]) % 10;
             }
-            q1.clear();
-            q1.addAll(q2);
-            q2.clear();
+            n--; // effectively the array shrinks by one
         }
 
-        return q1.peek();
+        return nums[0];
     }
 }
