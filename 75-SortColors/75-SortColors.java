c32d4e7fbec0b6954e8517ebefb317395b442abe
Aditya Garg <aditya444garg@gmail.com>
2025-10-13 21:11:29 +0530

    File updated at 10/13/2025, 9:11:26 PM

--- 75-SortColors/75-SortColors.java
+++ 75-SortColors/75-SortColors.java
@@ -1,6 +1,20 @@
-// Last updated: 10/13/2025, 9:07:39 PM
+// Last updated: 10/13/2025, 9:11:26 PM
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        int[] freq = new int[3];
+        for(int i=0;i<nums.length;i++){
+            freq[nums[i]]++;
+        }
+        int zeros = freq[0];
+        int ones = freq[1];
+        for(int i=0;i<zeros;i++){
+            nums[i] = 0;
+        }
+        for(int i=zeros;i<ones+zeros;i++){
+            nums[i] = 1;
+        }
+        for(int i=ones+zeros;i<nums.length;i++){
+            nums[i] = 2;
+        }
     }
 }
