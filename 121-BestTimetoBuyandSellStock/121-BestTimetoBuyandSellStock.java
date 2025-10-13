60ad437859e6d08e9e968fab9137fd148b5fcd10
Aditya Garg <aditya444garg@gmail.com>
2025-10-13 21:24:20 +0530

    File updated at 10/13/2025, 9:24:16 PM

--- 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.java
+++ 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.java
@@ -1,19 +1,12 @@
-// Last updated: 9/3/2025, 5:12:51 PM
+// Last updated: 10/13/2025, 9:24:16 PM
 class Solution {
     public int maxProfit(int[] prices) {
-        int n = prices.length;
+        int min = Integer.MAX_VALUE;
         int max = 0;
-        int left = 0;
-        int right = 1;
-        while(left<=right && right<n){
-            int a = prices[right] - prices[left];
-            if(a>max) max = a;
-            else if(prices[right] < prices[left]){
-                left = right;
-                right++;
-            }
-            else{
-                right++;
+        for(int i=0;i<prices.length;i++){
+            if(prices[i] < min) min = prices[i];
+            if(prices[i] - min  > max){
+                max = prices[i] - min;
             }
         }
         return max;
