607e960876ab0a49d061fbd26e080904de46e005
Aditya Garg <aditya444garg@gmail.com>
2025-10-13 21:26:10 +0530

    File updated at 10/13/2025, 9:26:06 PM

--- 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.java
+++ 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.java
@@ -1,14 +1,30 @@
-// Last updated: 10/13/2025, 9:24:16 PM
-class Solution {
+// Last updated: 10/13/2025, 9:26:06 PM
+public class Solution {
     public int maxProfit(int[] prices) {
-        int min = Integer.MAX_VALUE;
-        int max = 0;
-        for(int i=0;i<prices.length;i++){
-            if(prices[i] < min) min = prices[i];
-            if(prices[i] - min  > max){
-                max = prices[i] - min;
+        if (prices == null || prices.length < 2) return 0;
+
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int p : prices) {
+            // if current price is a new minimum, consider buying here
+            if (p < minPrice) {
+                minPrice = p;
+            } else {
+                // otherwise, consider selling today
+                int profitIfSoldToday = p - minPrice;
+                if (profitIfSoldToday > maxProfit) {
+                    maxProfit = profitIfSoldToday;
+                }
             }
         }
-        return max;
+        return maxProfit;
+    }
+
+    // quick main to test
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.maxProfit(new int[]{7,1,5,3,6,4})); // 5
+        System.out.println(s.maxProfit(new int[]{7,6,4,3,1}));   // 0
     }
 }
