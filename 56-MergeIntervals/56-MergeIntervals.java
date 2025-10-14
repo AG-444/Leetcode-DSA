464350aa64c25bef406c174e8831779ad29a4570
Aditya Garg <aditya444garg@gmail.com>
2025-10-14 15:00:28 +0530

    File updated at 10/14/2025, 3:00:27 PM

--- 56-MergeIntervals/56-MergeIntervals.java
+++ 56-MergeIntervals/56-MergeIntervals.java
@@ -1,31 +1,41 @@
-// Last updated: 9/5/2025, 9:20:48 PM
+// Last updated: 10/14/2025, 3:00:27 PM
 class Solution {
-    public int[][] merge(int[][] arr){
-        int n = arr.length;
-        Arrays.sort(arr, (a,b) -> {
+    public int[][] merge(int[][] intervals) {
+        Comparator<int[]> comp = (a,b) ->{
             if(a[0]!=b[0]) return Integer.compare(a[0],b[0]);
             else return Integer.compare(a[1],b[1]);
-        });
+        };
+        PriorityQueue<int[]> pq =  new PriorityQueue<>(comp);
+
+        for(int[] curr: intervals){
+            pq.offer(curr);
+        }
+
+        int[] first = pq.poll();
+        int start = first[0];
+        int end = first[1];
+
         List<int[]> ans = new ArrayList<>();
-        for(int i=0;i<n;i++){
-            int start = arr[i][0];
-            int end = arr[i][1];
-            if(ans.isEmpty()) ans.add(new int[]{start,end});
+        for(int i=0;i<intervals.length-1;i++){
+            int[] curr = pq.poll();
+            int newStart = curr[0];
+            int newEnd = curr[1];
+            if(start<=newStart && newStart<=end){
+                // System.out.println(start+ " "+end);
+                end = Math.max(end, newEnd);
+                // System.out.println("new = " + start+ " "+end);
+            }
             else{
-                int prevEnd = ans.get(ans.size()-1)[1];
-                if(prevEnd >= start){
-                    prevEnd = Math.max(prevEnd,end);
-                    ans.get(ans.size()-1)[1] = prevEnd;
-                }
-                else{
-                    ans.add(new int[]{start,end});
-                }
+                ans.add(new int[]{start,end});
+                // System.out.println("added = " + start+ " "+end);
+                start = newStart;
+                end = newEnd;
             }
         }
-        int[][] result = new int[ans.size()][2];
-        for (int i = 0; i < ans.size(); i++) {
-            result[i] = ans.get(i);
-        }
-        return result;
+        ans.add(new int[]{start,end});
+        
+
+        return ans.toArray(new int[ans.size()][]);
+
     }
 }
